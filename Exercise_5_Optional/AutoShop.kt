fe6ce4ee32679dd46aa6165af339a69b9cd2f4e8
marcus_rk <makj0005@stud.kea.dk>
2024-02-14 14:23:28 +0100

    Added functionality to AutoShop.kt class

--- Exercise_5_Optional/AutoShop.kt
+++ Exercise_5_Optional/AutoShop.kt
@@ -1,28 +1,30 @@
 package Exercise_5_Optional
 
-import Exercise_4.Circle
-import Exercise_4.Rectangle
-import Exercise_4.Shape
-import Exercise_4.Triangle
-
-class AutoShop {
+class AutoShop (
+    private val carList: MutableList<Car> = mutableListOf()
+){
+    fun showCarList(): Unit {
+        for (car in carList) {
+            println(car.toString())
+            println("Price: ${car.getSalePrice()}")
+            println()
+        }
+    }
 }
 
-
 fun main() {
-    // Create instances of Truck and Ford
-    val truck: Truck = Truck(212.8, 105000.0,"Yellow", 1980.5)
+    // Create instances of Truck, Ford, and Car
+    val truckOne: Truck = Truck(212.8, 105000.0,"Yellow", 1980.5)
     val fordOne: Ford = Ford(230.8, 249999.95, "Black", 1998, false)
     val fordTwo: Ford = Ford(232.5, 270000.0, "White", 2017, true)
-    val car: Car = Car(423.78, 1000000.00, "Purple")
+    val carOne: Car = Car(423.78, 1000000.00, "Purple")
 
     // Store the instances in a list (Possible because of polymorphism -> all Car)
-    val cars: List<Car> = listOf(truck, fordOne, fordTwo, car)
+    val cars: MutableList<Car> = mutableListOf(truckOne, fordOne, fordTwo, carOne)
 
-    // Display objects and getSalePrice() polymorphism override
-    for (car in cars) {
-        println(car.toString())
-        println("Price: ${car.getSalePrice()}")
-        println()
-    }
+    // Creating an autoShop instance with list of cars
+    val autoShop: AutoShop = AutoShop(cars)
+
+    // Display objects from autoshop carList and getSalePrice() polymorphism override
+    autoShop.showCarList()
 }
