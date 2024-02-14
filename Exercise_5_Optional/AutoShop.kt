624fdc4119f26741694116e81b4fddbe9bf5e050
marcus_rk <makj0005@stud.kea.dk>
2024-02-14 14:26:23 +0100

    change in truck price in AutoShop.kt

--- Exercise_5_Optional/AutoShop.kt
+++ Exercise_5_Optional/AutoShop.kt
@@ -14,7 +14,7 @@ class AutoShop (
 
 fun main() {
     // Create instances of Truck, Ford, and Car
-    val truckOne: Truck = Truck(212.8, 105000.0,"Yellow", 1980.5)
+    val truckOne: Truck = Truck(212.8, 100000.0,"Yellow", 1980.5)
     val fordOne: Ford = Ford(230.8, 249999.95, "Black", 1998, false)
     val fordTwo: Ford = Ford(232.5, 270000.0, "White", 2017, true)
     val carOne: Car = Car(423.78, 1000000.00, "Purple")
